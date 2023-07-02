2e40987a3cfd6c08a60a9b772dc4a96abd4d0f66
KonPim <100049831+Pimenov-Konstantin@users.noreply.github.com>
2023-07-02 18:35:32 +0300

    Add files via upload

---
+++ Seminar11/HomeWork/data.js
@@ -0,0 +1,45 @@
+const productsData = `
+[
+    {
+        "image": "img/price1.svg",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "currency": "$",
+        "price": "52.00"
+    },
+    {
+			"image": "img/price2.png",
+			"name": "ELLERY X M'O CAPSULE",
+			"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+			"currency": "$",
+			"price": "52.00"
+	},
+	{
+		"image": "img/price3.svg",
+		"name": "ELLERY X M'O CAPSULE",
+		"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"currency": "$",
+		"price": "52.00"
+},
+{
+	"image": "img/price4.svg",
+	"name": "ELLERY X M'O CAPSULE",
+	"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+	"currency": "$",
+	"price": "52.00"
+},
+{
+	"image": "img/price5.svg",
+	"name": "ELLERY X M'O CAPSULE",
+	"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+	"currency": "$",
+	"price": "52.00"
+},
+{
+	"image": "img/price6.svg",
+	"name": "ELLERY X M'O CAPSULE",
+	"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+	"currency": "$",
+	"price": "52.00"
+}
+]`
---
+++ Seminar11/data.js
@@ -0,0 +1,28 @@
+const dataInfo = `[
+	{
+	'brand': 'MANGO PEOPLE',
+	'category': 'T-SHIRT',
+	'currenncy': '$',
+	'price': '300',
+	'color': 'Red',
+	'size': 'XL',
+	'quantity': '2',
+	'image': './img/cart1.svg'
+},
+{
+	'brand': 'ELLERY X M'O',
+	'category': 'CAPSULE',
+	'currenncy': '$',
+	'price': '52',
+	'color': 'Black',
+	'size': 'XL',
+	'quantity': '1',
+	'image': './img/cart2.svg'
+}
+]`
+
+const product = document.querySelector('.cart-content');
+
+JSON.parse(dataProduct).forEach(item => {
+
+});
